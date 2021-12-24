a243ca1f68a48c4d42d1d2fddfba751425475b9b
allexkid <allexsantosrn@gmail.com>
2021-12-24 14:40:06 -0300

    Programa inicial
    Programas iniciais em Threads.

---
+++ src/code/MainThread.java
@@ -0,0 +1,15 @@
+package code;
+
+public class MainThread {
+
+	public static void main(String[] args) {
+
+		ThreadEx thread1 = new ThreadEx("Thread1");
+		ThreadEx thread2 = new ThreadEx("Thread2");
+
+		thread1.start();
+		thread2.start();
+		
+	}
+
+}
---
+++ src/code/ThreadEx.java
@@ -0,0 +1,15 @@
+package code;
+
+public class ThreadEx extends Thread {
+	
+	public ThreadEx(String nome) {
+		super(nome);		
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Hi, my name is: "+this.getName());		
+	}
+
+
+}
